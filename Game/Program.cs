353c19dd661675380d6b9cc1ec1b6b73c4d8facd
Paras Paras (1000113338) <PARA12@student.op.ac.nz>
2024-07-26 11:15:01 +1200

    added C# file

---
+++ Game/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace Game
+{
+    internal class Program
+    {
+        public static void PlaySound(string filePath)
+        {
+            SoundPlayer player = new SoundPlayer(filePath);
+            player.Load(); // Loads the .wav file into memory
+            player.Play(); // Plays the sound asynchronously
+        }
+        static void Main(string[] args)
+        {
+            PlaySound("file_example_WAV_1MG.wav");
+            Console.ReadLine();
+        }
+    }
+}
